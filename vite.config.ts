918561225570074a968c27185651975e4450bdd0
Lee <7932644+strahe@users.noreply.github.com>
2025-08-21 23:21:12 +0800

    feat: Add Vite plugin for type and lint checking

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,26 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import Pages from "vite-plugin-pages";
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(), 
+    vue(),
     tailwindcss(),
     Pages({
-      dirs: 'src/pages',
-      extensions: ['vue'],
-    })
+      dirs: "src/pages",
+      extensions: ["vue"],
+    }),
+    checker({
+      typescript: true,
+      vueTsc: true,
+      eslint: {
+        lintCommand: "eslint . --ext .vue,.js,.jsx,.cjs,.mjs,.ts,.tsx,.cts,.mts",
+        watchPath: "./src",
+        useFlatConfig: true,
+      },
+    }),
   ],
   resolve: {
     alias: {
